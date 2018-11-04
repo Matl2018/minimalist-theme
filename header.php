f1d4c36730df718c637fdfd880e09d3cfcce599e
Mat <lumierematerne11@gmail.com>
2018-11-04 11:23:42 +0000

    add logo image to the header

--- header.php
+++ header.php
@@ -24,14 +24,16 @@
 
 	<header id="masthead" class="site-header">
 		<div class="site-branding">
+		
 			<?php
 			if ( is_front_page() && is_home() ) :
 				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"> 
+				<img class=image-title src="<?php echo get_stylesheet_directory_uri(); ?>/img/logo-text.png" alt="Glasgow Bike Store logo" /> </a></h1>
 				<?php
 			else :
 				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><img class=image-title  src="<?php echo get_stylesheet_directory_uri(); ?>/img/logo-text.png" alt="Glasgow Bike Store logo" /></p>
 				<?php
 			endif;
 			$minimalist_theme_description = get_bloginfo( 'description', 'display' );
@@ -39,7 +41,10 @@
 				?>
 				<p class="site-description"><?php echo $minimalist_theme_description; /* WPCS: xss ok. */ ?></p>
 			<?php endif; ?>
-		</div><!-- .site-branding -->
+			
+		</div><!-- .site-branding -->	
+
+
 
 		<nav id="site-navigation" class="main-navigation">
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Menu', 'minimalist-theme' ); ?></button>
